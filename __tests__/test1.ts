edff0b5f70319726eef1d3c6fd360e21eae52ffc
Pavel Sharapov <gadpetrovich@gmail.com>
2023-04-09 19:00:31 +0300

    добавил тест для or

--- __tests__/test1.ts
+++ __tests__/test1.ts
@@ -1,13 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { and, lazy, unlazy } from '@src/baseFunctions';
+import { and, lazy, or, unlazy } from '@src/baseFunctions';
+
+const andValues = [
+  [false, false, false],
+  [false, true, false],
+  [true, false, false],
+  [true, true, true],
+];
+
+const orValues = [
+  [false, false, false],
+  [false, true, true],
+  [true, false, true],
+  [true, true, true],
+];
 
 describe('baseFunctions', () => {
-  test('функция and', () => {
-    const a = lazy(true);
-    const b = lazy(true);
-    const c = and(a, b);
-    const d = unlazy<boolean>(c);
-    expect(d).toBeTruthy();
+  test.each(andValues)('функция and(%d, %d = %d)', (a, b, expected) => {
+    const d = unlazy<boolean>(and(lazy(a), lazy(b)));
+    expect(d).toEqual(expected);
+  });
+
+  test.each(orValues)('функция or(%d, %d = %d)', (a, b, expected) => {
+    const d = unlazy<boolean>(or(lazy(a), lazy(b)));
+    expect(d).toEqual(expected);
   });
 });
