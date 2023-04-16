fe03448193ea69a3bf4447a1ac6a823eb0d04433
Pavel Sharapov <gadpetrovich@gmail.com>
2023-04-16 20:43:48 +0300

    обновил тесты для переполнения стека

--- __tests__/list.ts
+++ __tests__/list.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import type { LazyList } from '../src/index';
+import { fromList } from '../src/list';
 import {
   filter,
   foldl,
@@ -18,20 +18,11 @@ import {
   unlazy,
 } from '../src/index';
 
-function fromLazyList<T>(lazyList: LazyList<T>): T[] {
-  const resultList: T[] = [];
-  for (let value = lazyList(); value !== null; value = value.tail()) {
-    resultList.push(value.head());
-  }
-
-  return resultList;
-}
-
 describe('list', () => {
   test('toList', () => {
     const list = [1, 2, 3, 4];
     const lazyList = toList(list);
-    const resultList = fromLazyList(lazyList);
+    const resultList = fromList(lazyList);
     expect(resultList).toEqual(list);
   });
 
@@ -46,20 +37,26 @@ describe('list', () => {
   test('take', () => {
     const list = take(lazy(5), range(lazy(6)));
 
-    expect(fromLazyList(list)).toEqual([6, 7, 8, 9, 10]);
+    expect(fromList(list)).toEqual([6, 7, 8, 9, 10]);
   });
 
   test('skip', () => {
     const list = skip(lazy(2), take(lazy(5), range(lazy(6))));
 
-    expect(fromLazyList(list)).toEqual([8, 9, 10]);
+    expect(fromList(list)).toEqual([8, 9, 10]);
+  });
+
+  test('skip from short list', () => {
+    const list = skip(lazy(5), take(lazy(2), range(lazy(6))));
+
+    expect(list).toThrow('Извлечение значений из пустого списка');
   });
 
   test('filter', () => {
     const list = filter((v: number) => v % 2 === 0, range(lazy(0)));
 
     const result = take(lazy(5), list);
-    expect(fromLazyList(result)).toEqual([0, 2, 4, 6, 8]);
+    expect(fromList(result)).toEqual([0, 2, 4, 6, 8]);
   });
 
   test('map', () => {
@@ -67,7 +64,7 @@ describe('list', () => {
     const stringList = map((value: number) => String(value), sourceList);
     const result = take(lazy(5), stringList);
 
-    expect(fromLazyList(result).join('')).toEqual('12345');
+    expect(fromList(result).join('')).toEqual('12345');
   });
 
   test('head', () => {
@@ -88,7 +85,7 @@ describe('list', () => {
     const first5values = take(lazy(5), list);
     const revertedValues = reverse(first5values);
 
-    expect(fromLazyList(revertedValues)).toEqual([9, 8, 7, 6, 5]);
+    expect(fromList(revertedValues)).toEqual([9, 8, 7, 6, 5]);
   });
 
   test('last', () => {
@@ -139,70 +136,5 @@ describe('list', () => {
     const result = unlazy(foldl(x => x, lazy(3), list));
 
     expect(result).toEqual(3);
-  });
-});
-
-// Проверка на переполнение стека
-describe('looong list', () => {
-  const count = 100000;
-  const longList = [...Array(count).keys()];
-
-  test(`take ${count}`, () => {
-    const list = take(lazy(count), range(lazy(0)));
-
-    expect(fromLazyList(list)).toEqual(longList);
-  });
-
-  test(`map ${count}`, () => {
-    const sourceList = range(lazy(0));
-    const stringList = map((value: number) => String(value), sourceList);
-    const result = take(lazy(count), stringList);
-    const expectedList = longList.map(v => String(v)).join('');
-
-    expect(fromLazyList(result).join('')).toEqual(expectedList);
-  });
-
-  test(`foldr ${count}`, () => {
-    const sourceList = range(lazy(0));
-    const takedList = take(lazy(count), sourceList);
-    const sum = foldr((a: number, b: number) => a + b, lazy(0), takedList);
-    const expectedSum = longList.reduce((a, b) => a + b);
-
-    expect(unlazy(sum)).toEqual(expectedSum);
-  });
-
-  test(`foldl ${count}`, () => {
-    const sourceList = range(lazy(0));
-    const takedList = take(lazy(count), sourceList);
-    const sum = foldl((a: number, b: number) => a + b, lazy(0), takedList);
-    const expectedSum = longList.reduce((a, b) => a + b);
-
-    expect(unlazy(sum)).toEqual(expectedSum);
-  });
-
-  test(`length ${count}`, () => {
-    const sourceList = range(lazy(0));
-    const takedList = take(lazy(count), sourceList);
-    const len = length(takedList);
-
-    expect(unlazy(len)).toEqual(count);
-  });
-
-  test(`reverse ${count}`, () => {
-    const sourceList = range(lazy(0));
-    const takedList = take(lazy(count), sourceList);
-    const reversedList = reverse(takedList);
-    const expectedList = longList.slice().reverse();
-
-    expect(fromLazyList(reversedList)).toEqual(expectedList);
-  });
-
-  test(`filter ${count}`, () => {
-    const sourceList = range(lazy(0));
-    const filteredList = filter((x: number) => x % 2 === 0, sourceList);
-    const takedList = take(lazy(count), filteredList);
-    const expectedList = longList.map(value => value * 2);
-
-    expect(fromLazyList(takedList)).toEqual(expectedList);
   });
 });
---
+++ __tests__/longList.ts
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { fromList } from '../src/list';
+import {
+  filter,
+  foldl,
+  foldr,
+  lazy,
+  length,
+  map,
+  range,
+  reverse,
+  skip,
+  take,
+  unlazy,
+} from '../src/index';
+
+// Проверка на переполнение стека
+describe('looong list', () => {
+  const count = 100000;
+  const longList = [...Array(count).keys()];
+
+  test(`take ${count}`, () => {
+    const list = take(lazy(count), range(lazy(0)));
+
+    expect(fromList(list)).toEqual(longList);
+  });
+
+  test(`long skip`, () => {
+    const list = skip(lazy(count), range(lazy(0)));
+    const takedList = take(lazy(1), list);
+
+    expect(fromList(takedList)).toEqual([count]);
+  });
+
+  test(`long map`, () => {
+    const sourceList = range(lazy(0));
+    const stringList = map((value: number) => String(value), sourceList);
+    const result = take(lazy(count), stringList);
+    const expectedList = longList.map(v => String(v)).join('');
+
+    expect(fromList(result).join('')).toEqual(expectedList);
+  });
+
+  test(`long foldr`, () => {
+    const sourceList = range(lazy(0));
+    const takedList = take(lazy(count), sourceList);
+    const sum = foldr((a: number, b: number) => a + b, lazy(0), takedList);
+    const expectedSum = longList.reduce((a, b) => a + b);
+
+    expect(unlazy(sum)).toEqual(expectedSum);
+  });
+
+  test(`long foldl`, () => {
+    const sourceList = range(lazy(0));
+    const takedList = take(lazy(count), sourceList);
+    const sum = foldl((a: number, b: number) => a + b, lazy(0), takedList);
+    const expectedSum = longList.reduce((a, b) => a + b);
+
+    expect(unlazy(sum)).toEqual(expectedSum);
+  });
+
+  test(`long length`, () => {
+    const sourceList = range(lazy(0));
+    const takedList = take(lazy(count), sourceList);
+    const len = length(takedList);
+
+    expect(unlazy(len)).toEqual(count);
+  });
+
+  test(`long reverse`, () => {
+    const sourceList = range(lazy(0));
+    const takedList = take(lazy(count), sourceList);
+    const reversedList = reverse(takedList);
+    const expectedList = longList.slice().reverse();
+
+    expect(fromList(reversedList)).toEqual(expectedList);
+  });
+
+  test(`long filter`, () => {
+    const sourceList = range(lazy(0));
+    const filteredList = filter((x: number) => x % 2 === 0, sourceList);
+    const takedList = take(lazy(count), filteredList);
+    const expectedList = longList.map(value => value * 2);
+
+    expect(fromList(takedList)).toEqual(expectedList);
+  });
+});
